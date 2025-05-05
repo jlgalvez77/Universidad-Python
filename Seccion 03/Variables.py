c080969db36fbad513da3694273bac696962454c
Jose Galvez <jlgalvez@gmail.com>
2025-05-05 19:49:40 +0200

    Inicio

---
+++ Seccion 03/Variables.py
@@ -0,0 +1,25 @@
+# Variables en Python
+
+# Declaración e inicialización de variables
+edad = 48
+altura = 1.80
+pais = 'España'
+
+# Acceder a las variables
+print(f'Edad: {edad}')
+print(f'Altura: {altura}')
+print(f'Pais: {pais}')
+
+# Modificar el valor de una variable
+edad = 49
+altura = 1.81
+print(f'Edad: {edad}')
+print(f'Altura: {altura}')
+
+# En Python el tipo es dinamico
+edad = 'Cuarentainueve'
+print(f'Edad: {edad}')
+
+# Si accedemos a una variable no declarada dará error
+telefono = 999999999
+print(f'Telefono: {telefono}')
